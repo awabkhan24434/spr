088b5698214c82586ff09dcb453ffadf139238d1
awabkhan24434 <awabkhan065@gmail.com>
2019-11-21 16:08:19 +0500

    First commit

---
+++ app/Http/Controllers/awab.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class awab extends Controller
+{
+    //
+}
---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+@section('title','about')
+@section('navbar')
+
+@parent
+
+ @endssection
